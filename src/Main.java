198c2e68735a1aea4ca76f51e7d177a2c9c72846
leetgrind <leetgrind.org@gmail.com>
2023-11-15 18:30:06 -0500

    added sleep

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,27 @@ public class Main {
 
         System.out.println("Inside: " + Thread.currentThread().getName());
 
+        threadSleep();
+
+
+    }
+
+    static void threadExtend() {
         Thread thread = new ThreadExample();
 
         System.out.println("Starting thread");
         thread.start();
+    }
 
+    static void threadRunnable() {
         Runnable runnable = new RunnableEx();
 
         Thread runnableThread = new Thread(runnable);
         System.out.println("Starting runnable thread");
         runnableThread.start();
+    }
+
+    static void threadLambda() {
 
         Runnable runnableLambda = () ->
                 System.out.println("Inside : " + Thread.currentThread().getName());
@@ -21,5 +32,25 @@ public class Main {
         System.out.println("Starting runnable thread lambda");
         runnableThreadLambda.start();
 
+    }
+
+    static void threadSleep() {
+
+        Runnable runnable = () -> {
+            System.out.println("Inside : " + Thread.currentThread().getName());
+            System.out.println("Sleeping for 500ms");
+            try {
+                Thread.sleep(2000);
+                System.out.println("Running after sleeping");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+        System.out.println("Thread is back 1");
+        System.out.println("Thread is back 2");
+        System.out.println("Thread is back 3");
     }
 }
