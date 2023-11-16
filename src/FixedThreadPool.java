53e98c4a3bf1a04a3e5daeb8aaf81978a3142a97
leetgrind <leetgrind.org@gmail.com>
2023-11-15 19:57:19 -0500

    added thread pool

--- src/FixedThreadPool.java
+++ src/FixedThreadPool.java
@@ -1,2 +1,30 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class FixedThreadPool {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        Runnable runnable = () -> {
+            System.out.println("Inside thread " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Thread 1 finished");
+        };
+
+
+        executorService.submit(runnable);
+
+        Runnable runnable2 = () ->
+                System.out.println("Inside thread " + Thread.currentThread().getName());
+
+        executorService.submit(runnable2);
+
+        executorService.shutdown();
+
+    }
 }
