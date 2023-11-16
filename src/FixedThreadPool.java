7e6ea6cfc588190d60bb5b04a7d80a96e5079ecc
leetgrind <leetgrind.org@gmail.com>
2023-11-15 20:20:18 -0500

    scheduled executor

--- src/FixedThreadPool.java
+++ src/FixedThreadPool.java
@@ -1,15 +1,16 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class FixedThreadPool {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
         Runnable runnable = () -> {
             System.out.println("Inside thread " + Thread.currentThread().getName());
             try {
-                Thread.sleep(1000);
+                Thread.sleep(10000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -25,6 +26,10 @@ public class FixedThreadPool {
         executorService.submit(runnable2);
 
         executorService.shutdown();
+
+        if(!executorService.awaitTermination(1, TimeUnit.SECONDS)) {
+            executorService.shutdownNow();
+        }
 
     }
 }
---
+++ src/ScheduleThreadPool.java
@@ -0,0 +1,28 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduleThreadPool {
+
+    public static void main(String[] args) {
+        // execute task periodically
+        // execute after delay
+        ScheduledExecutorService scheduledExecutorService =
+                Executors.newScheduledThreadPool(1);
+
+        Runnable task = () ->
+                System.out.println("Current time: " + System.currentTimeMillis());
+
+
+        System.out.println("Submission time " + System.currentTimeMillis());
+
+        scheduledExecutorService.schedule(task, 500, TimeUnit.MILLISECONDS);
+
+
+        scheduledExecutorService.scheduleAtFixedRate(task, 0, 2, TimeUnit.SECONDS);
+
+        //scheduledExecutorService.shutdown();
+
+
+    }
+}
