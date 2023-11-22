acb599249b623dbcfdc33cd81c44e47ba60de546
ace <ace@aces-MacBook-Pro.local>
2023-11-22 18:51:10 -0500

    sync

---
+++ src/ThreadRaceCondition.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadRaceCondition {
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        Counter counter =  new Counter();
+
+        for(int i=0; i<10; i++) {
+            executorService.submit(counter::increase);
+        }
+
+        executorService.shutdown();
+
+        System.out.println(counter.getCount());
+
+    }
+}
+
+
+class Counter {
+    private int count = 0;
+
+    public void increase() {
+        synchronized (this) {
+            ++this.count;
+        }
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+}
