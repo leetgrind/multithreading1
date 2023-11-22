c3776d6243cb2907f31f94fdb2df3b9d506e5935
ace <ace@aces-MacBook-Pro.local>
2023-11-22 17:41:29 -0500

    Added future cancel

--- src/CallableMain.java
+++ src/CallableMain.java
@@ -13,7 +13,7 @@ public class CallableMain {
 
         Callable<String> callable = () -> {
             System.out.println(Thread.currentThread().getName()+ " Processing");
-            Thread.sleep(2000);
+            Thread.sleep(100000);
             System.out.println(Thread.currentThread().getName()+ " Processing finished");
             return "Thread completed";
         };
@@ -22,6 +22,23 @@ public class CallableMain {
 
         System.out.println(Thread.currentThread().getName() + " Callable submitted");
         System.out.println("Main thread is blocked");
+
+        int i = 0;
+        while(!future.isDone()) {
+            System.out.println("Waiting for task to finish");
+            Thread.sleep(200);
+            if(i==5) {
+                future.cancel(true);
+            }
+            i++;
+        }
+
+        if(future.isCancelled()) {
+            System.out.println("Waited too long. Exiting the program");
+            executorService.shutdown();
+            return;
+        }
+
         String result = future.get();
         System.out.println(Thread.currentThread().getName() + " Future is complete");
         System.out.println("Result: " + result);
